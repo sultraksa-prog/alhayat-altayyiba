04a2cff4c81f38f8ac03ce762ba53f4ca876df43
sultraksa-prog <sultraksa@gmail.com>
2026-09-23 21:59:10 +0300

    Remove skipWaiting from install event
    Remove skipWaiting call during service worker installation.

--- sw.js
+++ sw.js
@@ -19,7 +19,6 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
